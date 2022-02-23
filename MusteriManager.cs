11f065d97bdbd56ebc1d3035999600ccb20bb70b
Mehmet ORTABURUN <mehmet.ortaburun@gmail.com>
2022-02-23 22:43:10 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    // iş sınıfları
+    class MusteriManager
+    {
+         
+        public void Ekle(Musteri musteri)
+        {
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Mehmet";
+            musteri1.Soyadi = "Ortaburun";
+            musteri1.TcNo = "12345678912";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "kodlama.ıo";
+            musteri2.VergiNo = "9876543210";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            //Musteri sınıfından kalıtılmıs tüm değerleri ekler
+            // Ekle() metodu parametre olarak Musteri sınıfından bir nesne alır
+            // Polimorfizim işlemi 
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+
+
+
+
+
+        }
+    }
+}
